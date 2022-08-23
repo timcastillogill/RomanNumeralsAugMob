78a5e8decb751ff76e7ffce377faaf303ecea5e0
MattW <mattnwarnock@yahoo.co.uk>
2022-08-23 16:36:59 +0100

    Green: refactor to while

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,19 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanNumerals {
 
 
 	public String convert(int decimal) {
 
 		String result = "";
-		if (decimal == 50) {
+
+		while (decimal >= 100) {
+			result += "C";
+			decimal -= 100;
+		}
+
+		while (decimal >= 50) {
 			result += "L";
 			decimal -= 50;
 		}
+
 		while (decimal >= 10) {
 			result += "X";
 			decimal -= 10;
 		}
 
-		if (decimal >= 5) {
+		while (decimal >= 5) {
 			result += "V";
 			decimal -= 5;
 		}
