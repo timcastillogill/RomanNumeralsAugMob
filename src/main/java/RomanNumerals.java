fcda3993c85ce397de778feec762a1fda860b474
timcastillogill <timgillviola@gmail.com>
2022-08-23 14:42:51 +0100

    GREEN: passing with 3

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -5,6 +5,9 @@ public class RomanNumerals {
 		if (decimal == 2) {
 			return "II";
 		}
+		if (decimal == 3) {
+			return "III";
+		}
 		return "I";
 	}
 }
--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -1,9 +1,9 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RomanNumeralsTest {
-
 	@Test
 	public void
 	given_decimal_returns_roman_numeral_string() {
