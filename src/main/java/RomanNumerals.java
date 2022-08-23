78b8ab3124e54e31473dc8e88ba92747262cfaaa
timcastillogill <timgillviola@gmail.com>
2022-08-23 14:51:58 +0100

    Refactor: Before Each in Tests and repeat in Production

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -2,12 +2,6 @@ public class RomanNumerals {
 
 
 	public String convert(int decimal) {
-		if (decimal == 2) {
-			return "II";
-		}
-		if (decimal == 3) {
-			return "III";
-		}
-		return "I";
+		return "I".repeat(decimal);
 	}
 }
--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -4,12 +4,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RomanNumeralsTest {
+
+	private RomanNumerals romanNumerals;
+	@BeforeEach
+	void setup() {
+		romanNumerals = new RomanNumerals();
+	}
 	@Test
 	public void
 	given_decimal_returns_roman_numeral_string() {
-		assertEquals("I", new RomanNumerals().convert(1));
-		assertEquals("II", new RomanNumerals().convert(2));
-		assertEquals("III", new RomanNumerals().convert(3));
+		assertEquals("I", romanNumerals.convert(1));
+		assertEquals("II", romanNumerals.convert(2));
+		assertEquals("III", romanNumerals.convert(3));
 	}
 
 }
